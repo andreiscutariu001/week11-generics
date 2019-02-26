5bebaef0b53945d0a5c6db75cc4dbce1bdd1c560
Andrei Scutariu <andreiscutariu92@gmail.com>
2019-02-26 19:35:09 +0200

    calculator app

---
+++ Week11Generics/CalculatorApp/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    public class Calculator 
+    {
+        public int Add(int a, int b) 
+        {
+            return a + b; 
+        }
+
+        public decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+
+    public class CalculatorV2
+    {
+        public T Add<T>(T param1, T param2) where T : class, IHaveAnAddMethod<T>
+        {
+            return param1.Add(param2);
+        }
+    }
+
+    public interface IHaveAnAddMethod<T>
+    {
+        T Add(T p);
+    }
+}
---
+++ Week11Generics/CalculatorApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new CalculatorV2();
+
+            //var param1 = new Value(1);
+            //var param2 = new Value(2);
+
+            //var x = c.Add(param1, param2);
+
+            var s1 = new Student("And");
+            var s2 = new Student("rei");
+            
+            var s3 = c.Add(s1, s2);
+
+            var list = new List<Student>();
+        }
+    }
+
+    public class Value : IHaveAnAddMethod<Value>
+    {
+        public int Val { get; set; }
+
+        public Value(int a)
+        {
+            Val = a;
+        }
+
+        public Value Add(Value p)
+        {
+            var i = this.Val + p.Val;
+
+            return new Value(i);
+        }
+    }
+
+    public class Student : IHaveAnAddMethod<Student>
+    {
+        public Student(string a)
+        {
+            this.Name = a;
+        }
+
+        public string Name { get; set; }
+
+        public Student Add(Student p)
+        {
+            var res = this.Name + p.Name;
+
+            return new Student(res);
+        }
+    }
+}
